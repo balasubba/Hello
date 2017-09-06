8e5d9f9e4378e7c420d7e97ae33e6129193e25ec
balasubba <vaddebala6@gmail.com>
2017-09-06 14:18:10 +0530

    Hi

--- Hello.java
+++ Hello.java
@@ -5,9 +5,9 @@ public class Hello {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i=0;i<=3;i++)
+		for(int i=0;i<=20;i++)
 		{
-			System.out.println("hello word ...."+i);
+			System.out.println("helloworld ...."+i);
 		}
 
 	}
